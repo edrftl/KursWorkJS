0c2ece2036930a36448279b2edac7d5bba51897e
puzyrko2007 <puzyrko2007@gmail.com>
2024-03-07 19:58:21 +0200

    01

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
 const filmList = document.querySelector('.row');
-const searchInp = document.forms.search_form.elements.search_input;
+const addVideo = document.querySelector('#add-video');
 const films = [];
 
-document.getElementById('searchByNameBtn').onclick = () => {
-    const searchTerm = searchInp.value.toLowerCase();
-    loadProductsToTable(films.filter(x => x.name.toLowerCase().includes(searchTerm)));
-};
+const form = document.forms.create_form;
+const nameIn = form.elements.name;
+const yearIn = form.elements.year;
+const tagsIn = form.elements.tags;
+const authorIn = form.elements.author;
+const countryIn = form.elements.country;
+const timeIn = form.elements.time;
+const ratingIn = form.elements.rating;
+const urlIn = form.elements.url;
 
-function addFilmToList(item) {
-    filmList.innerHTML += `
-        <div class="col mb-4">
-            <div class="card bg-dark text-light">
-                <img src="${item.imageUrl}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${item.name}</h5>
-                    <p class="card-text">${item.year}</p>
-                </div>
-            </div>
-        </div>`;
+// document.getElementById('searchByNameBtn').onclick = () => {
+//     const searchTerm = form.elements.search_input.value.toLowerCase();
+//     loadProductsToTable(films.filter(x => x.name.toLowerCase().includes(searchTerm)));
+// };
+
+addVideo.onclick = (event) => {
+    event.preventDefault();
+    let item = new Film(nameIn.value, +yearIn.value, tagsIn.value.split(' '), authorIn.value, countryIn.value, timeIn.value, ratingIn.value, urlIn.value);
+    films.push(item);
+    console.log(films);
+    // addFilmToList(item);
 }
+
+// function addFilmToList(item) {
+//     filmList.innerHTML += `
+//         <div class="col mb-4">
+//             <div class="card bg-dark text-light">
+//                 <img src="${item.url}" class="card-img-top" alt="...">
+//                 <div class="card-body">
+//                     <h5 class="card-title">${item.name}</h5>
+//                     <p class="card-text">${item.year}</p>
+//                 </div>
+//             </div>
+//         </div>`;
+// }
 
 function loadProductsToTable(items) {
     filmList.innerHTML = "";
@@ -28,10 +46,113 @@ function loadProductsToTable(items) {
 }
 
 class Film {
-    static count = 0;
-    constructor(name, year, imageUrl) {
+    constructor(name, year, tags, author, country, time, rating, url) {
         this.name = name;
         this.year = year;
-        this.imageUrl = imageUrl;
+        this.tags = tags;
+        this.author = author;
+        this.country = country;
+        this.time = time;
+        this.rating = rating;
+        this.url = url;
+    }
+
+    addTags(tag) {
+        this.tags.push(tag);
     }
 }
+
+
+
+
+
+
+
+
+
+// const filmList = document.querySelector('.row');
+// const addVideo = document.querySelector('#add-video');
+// const films = [];
+
+
+// const form = document.forms.create_video;
+// const nameIn = form.elements.name;
+// const yearIn = form.elements.year;
+// const tagsIn = form.elements.tags;
+// const authorIn = form.elements.author;
+// const countryIn = form.elements.country;
+// const timeIn = form.elements.time;
+// const ratingIn = form.elements.rating;
+// const urlIn = form.elements.url;
+
+
+// document.getElementById('searchByNameBtn').onclick = () => {
+//     const searchTerm = searchInp.value.toLowerCase();
+//     loadProductsToTable(films.filter(x => x.name.toLowerCase().includes(searchTerm)));
+// };
+
+
+// addVideo.onclick = (event) => {
+
+//     event.preventDefault();
+//     // if (nameIn.value && urlIn.value) {
+//     let item = new Film(nameIn.value, +yearIn.value, tagsIn.value.split(' '), authorIn.value, countryIn.value, timeIn.value, ratingIn.value, urlIn.value);
+//     films.push(item);
+//     addFilmToList(item);
+//     // console.log(films);
+//     // } else
+//     //     alert("Enter all info");
+// }
+
+// function addFilmToList(item) {
+//     filmList.innerHTML += `
+//         <div class="col mb-4">
+//             <div class="card bg-dark text-light">
+//                 <img src="${item.imageUrl}" class="card-img-top" alt="...">
+//                 <div class="card-body">
+//                     <h5 class="card-title">${item.name}</h5>
+//                     <p class="card-text">${item.year}</p>
+//                 </div>
+//             </div>
+//         </div>`;
+// }
+
+// function loadProductsToTable(items) {
+//     filmList.innerHTML = "";
+//     for (const film of items) {
+//         addFilmToList(film);
+//     }
+// }
+
+
+
+// class Film {
+//     static count = 0;
+//     constructor(name, year, tags, author, country, time, rating, url) {
+//         this.name = name;
+//         this.year = year;
+//         this.tags = tags;
+//         this.author = author;
+//         this.country = country;
+//         this.time = time;
+//         this.rating = rating;
+//         this.url = url;
+//     }
+
+
+//     addTags(tag) {
+//         this.tags.push(tag);
+//     }
+// }
+
+
+
+
+// // class Film {
+// //     static count = 0;
+// //     constructor(name, year, imageUrl) {
+// //         this.name = name;
+// //         this.year = year;
+// //         this.imageUrl = imageUrl;
+// //     }
+// // }
